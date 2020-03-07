57d48514e1559bac52bfacccc564da88ad7f2f1f
arikui1911 <arikui.ruby@gmail.com>
2020-03-07 22:15:18 +0900

    raise YAJP::ParseError for parse error

--- lib/yajp/parser.rb
+++ lib/yajp/parser.rb
@@ -12,5 +12,43 @@ module YAJP
       [:json, @lexer.filename, @core.parse]
     end
   end
+
+  class ParseError < RuntimeError
+    def initialize(cause_token, msg)
+      super msg
+      @cause_token = cause_token
+    end
+
+    attr_reader :cause_token
+  end
+
+  class ParseCore
+    def initialize(lexer)
+      @lexer = lexer
+    end
+
+    def parse
+      do_parse
+    end
+
+    private
+
+    def next_token
+      token = @lexer.read
+      return [false, nil] if token.kind == :EOF
+      [token.kind, token]
+    end
+
+    def pos(token)
+      [token.line, token.col]
+    end
+
+    def on_error(id, token, stack)
+      insp = token.value.inspect
+      insp = "#{insp}(#{token.kind})" unless token.kind == token.value
+      raise ParseError.new(token, "#{@lexer.filename}:#{token.line}:#{token.col}: parse error on #{insp})")
+    end
+  end
+  private_constant :ParseCore
 end
 
