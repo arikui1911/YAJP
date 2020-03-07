0d4c76c7907a5bfeebcedc1edcd2c4c56eb981df
arikui1911 <arikui.ruby@gmail.com>
2020-03-07 22:49:55 +0900

    write some YARD

--- lib/yajp.rb
+++ lib/yajp.rb
@@ -3,13 +3,34 @@ require 'yajp/parser'
 require 'yajp/compiler'
 require 'pathname'
 
+# YAJP is "Yet Another JSON Parser".
+#
+# YAJP is written in order to practice using pattern matching,
+# so it is not considered to be a practical library.
+#
 module YAJP
+  # Parse string which represent JSON source code.
+  #
+  # @param [String] src JSON source string
+  # 
+  # @return [Object] value from JSON
+  #
+  # @raise [ParseError] JSON might contain syntax error
+  #
   def self.parse_string(src, **options)
     raise ArgumentError, "#{src} is not compatible to String" unless src.respond_to?(:to_str)
     ast = do_parse(src.to_str, Object.instance_method(:inspect).bind(src).call)
     do_compile ast, options
   end
 
+  # Parse JSON file.
+  #
+  # @param [String] path file path for JSON file
+  # 
+  # @return [Object] value from JSON
+  #
+  # @raise [ParseError] JSON might contain syntax error
+  #
   def self.parse_file(path, **options)
     raise ArgumentError, "#{path} is not compatible to file path" unless path.respond_to?(:to_path)
     path = path.to_path
